15b9dec2b66c391f1689c648f99610df2b29aca4
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-03-30 12:24:12 -0400

    Delete sanity directory

--- sanity/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {productType} from './productType'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, productType],
-}
