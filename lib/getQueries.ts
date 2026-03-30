e4f3e1a3e6fc417b4c0951d73a1b1653ad41b537
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2026-03-30 12:31:50 -0400

    eliminacion saniity

--- lib/getQueries.ts
+++
@@ -1,8 +0,0 @@
-import {client} from '@/sanity/lib/client'
-import { type SanityDocument } from "next-sanity";
-import {PRODUCTS_QUERY} from '@/sanity/lib/queries'
-
-export async function getProducts() {
-  const res = await client.fetch<SanityDocument[]>(PRODUCTS_QUERY)
-  return {res}
-}
